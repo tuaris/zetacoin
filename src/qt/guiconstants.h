1b21845f4d0afba66ca013b97369636c319298da
Daniel Morante <daniel@morante.net>
2026-03-25 15:25:28 -0400

    qt: rebrand GUI icons and app names for Zetacoin Core
    - Replace all icon images (PNG, ICO, ICNS, XPM) with Zetacoin zeta symbol
    - Update QAPP_ORG_NAME, QAPP_ORG_DOMAIN, QAPP_APP_NAME_* in guiconstants.h
    - Icons sourced from original zetacoin/zetacoin repo (v0.13.2.1)

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -46,13 +46,13 @@ static const int TOOLTIP_WRAP_THRESHOLD = 80;
 /* Number of frames in spinner animation */
 #define SPINNER_FRAMES 36
 
-#define QAPP_ORG_NAME "Bitcoin"
-#define QAPP_ORG_DOMAIN "bitcoin.org"
-#define QAPP_APP_NAME_DEFAULT "Bitcoin-Qt"
-#define QAPP_APP_NAME_TESTNET "Bitcoin-Qt-testnet"
-#define QAPP_APP_NAME_TESTNET4 "Bitcoin-Qt-testnet4"
-#define QAPP_APP_NAME_SIGNET "Bitcoin-Qt-signet"
-#define QAPP_APP_NAME_REGTEST "Bitcoin-Qt-regtest"
+#define QAPP_ORG_NAME "Zetacoin"
+#define QAPP_ORG_DOMAIN "github.com/tuaris/zetacoin"
+#define QAPP_APP_NAME_DEFAULT "Zetacoin-Qt"
+#define QAPP_APP_NAME_TESTNET "Zetacoin-Qt-testnet"
+#define QAPP_APP_NAME_TESTNET4 "Zetacoin-Qt-testnet4"
+#define QAPP_APP_NAME_SIGNET "Zetacoin-Qt-signet"
+#define QAPP_APP_NAME_REGTEST "Zetacoin-Qt-regtest"
 
 /* One gigabyte (GB) in bytes */
 static constexpr uint64_t GB_BYTES{1000000000};
