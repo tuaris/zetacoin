e92510462cd88fc182302d0f954d248f10adc810
Daniel Morante <daniel@morante.net>
2026-03-29 21:31:37 -0400

    cleanup: remove unused empty seed arrays from chainparamsseeds.h

--- src/chainparamsseeds.h
+++ src/chainparamsseeds.h
@@ -5,15 +5,11 @@
  *
  * Each line contains a BIP155 serialized (networkID, addr, port) tuple.
  * networkID 0x01 = IPv4, addrlen 0x04, then 4 addr bytes, then 2 port bytes (big-endian)
+ *
+ * Testnet4, signet, and regtest use vFixedSeeds.clear() in chainparams.cpp.
  */
 static const uint8_t chainparams_seed_main[] = {
     // 63.247.147.166:17333
     0x01,0x04,0x3f,0xf7,0x93,0xa6,0x43,0xb5,
-};
-static const uint8_t chainparams_seed_test[] = {
-};
-static const uint8_t chainparams_seed_testnet4[] = {
-};
-static const uint8_t chainparams_seed_signet[] = {
 };
 #endif // BITCOIN_CHAINPARAMSSEEDS_H
